2dd619bc58ba6f82904319c232bcd7d5b7fe9f24
esteban-vm <estebanvm90@outlook.com>
2024-10-20 11:51:13 -0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Image from 'next/image'
+import { Hero } from 'rsc-daisyui'
+import { Form } from '@/components'
+import backgroundImg from '@/images/background.jpg'
+import formImg from '@/images/form.jpg'
+
 export default function Home() {
-  return <section></section>
+  return (
+    <Hero className='relative min-h-full w-screen'>
+      <Image alt='' className='object-cover' src={backgroundImg} fill />
+      <Hero.Overlay className='z-10 bg-neutral/90' />
+      <Hero.Content className='z-20 flex-col md:flex-row'>
+        <Image
+          alt='food'
+          className='w-3/4 rounded-box grayscale transition-all delay-500 duration-200 hover:grayscale-0 md:w-1/3'
+          src={formImg}
+        />
+        <Form />
+      </Hero.Content>
+    </Hero>
+  )
 }
