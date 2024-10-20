ab4cd2eb538ca245f0503003ac44c776bd05d2bd
esteban-vm <estebanvm90@outlook.com>
2024-10-20 11:49:27 -0300

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+import localFont from 'next/font/local'
+import { clsx } from '@/lib/utils'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -11,8 +13,27 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
       <body suppressHydrationWarning>
-        <main className='container mx-auto h-screen min-h-[48rem] border border-primary'>{children}</main>
+        <main className={clsx('h-screen w-full', alegreya.variable, pacifico.variable)}>{children}</main>
       </body>
     </html>
   )
 }
+
+const alegreya = localFont({
+  variable: '--font-alegreya',
+  display: 'swap',
+  fallback: ['serif'],
+  src: [
+    { path: '../../public/fonts/Alegreya-VariableFont_wght.woff2', style: 'normal' },
+    { path: '../../public/fonts/Alegreya-Italic-VariableFont_wght.woff2', style: 'italic' },
+  ],
+})
+
+const pacifico = localFont({
+  variable: '--font-pacifico',
+  display: 'swap',
+  fallback: ['cursive'],
+  weight: '400',
+  style: 'normal',
+  src: '../../public/fonts/Pacifico-Regular.woff2',
+})
