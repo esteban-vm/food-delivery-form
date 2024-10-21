3edda08811488e53b8a50254dc82d5d5468bec56
esteban-vm <estebanvm90@outlook.com>
2024-10-21 11:25:05 -0300

    refactor utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 const twClsx = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
-const splitCamelCase = (str: string) => str.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase()
+const splitCamelCase = (str: string) => str.replace(/([a-z])([A-Z])/g, '$1 $2')
 const getLastKey = (str: string) => str.substring(str.lastIndexOf('.') + 1)
 
 export { twClsx as clsx }
