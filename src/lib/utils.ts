18e43dab57c891ad31c1192796aca25199f22b29
esteban-vm <estebanvm90@outlook.com>
2024-10-21 09:34:51 -0300

    create getLabelText util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,32 @@ import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 const twClsx = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+const splitCamelCase = (str: string) => str.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase()
 
 export { twClsx as clsx }
 
-export const splitCamelCase = (str: string) => str.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase()
+export const getLabelText = (label: FormLabel) => {
+  let labelText = ''
+
+  switch (label) {
+    case 'address':
+      break
+    case 'deliveryTime':
+    case 'paymentMethod':
+      labelText = `${splitCamelCase(label)}:`
+      break
+    case 'orderNumber':
+      labelText = 'Order no.'
+      break
+    case 'cellphone':
+    case 'email':
+    case 'name':
+      labelText = `your ${label}:`
+      break
+    default:
+      labelText = label
+      break
+  }
+
+  return labelText
+}
