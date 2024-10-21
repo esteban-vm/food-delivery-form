edc2c58786d2a34ef1377a70cedd4bbae83ceb11
esteban-vm <estebanvm90@outlook.com>
2024-10-20 21:00:03 -0300

    add form row

---
+++ src/components/atoms/form-row.tsx
@@ -0,0 +1,3 @@
+export default function FormRow(props: JSX.IntrinsicElements['div']) {
+  return <div {...props} className='flex w-full flex-col gap-1 md:flex-row' />
+}
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as FormButton } from './form-button'
 export { default as FormLabel } from './form-label'
+export { default as FormRow } from './form-row'
 export { default as FormTitle } from './form-title'
