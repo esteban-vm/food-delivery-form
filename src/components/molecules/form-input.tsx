1b34cd065853ac2d05f87e4aeeba65119fe2e421
esteban-vm <estebanvm90@outlook.com>
2024-10-22 10:47:55 -0300

    fix id

--- src/components/molecules/form-input.tsx
+++ src/components/molecules/form-input.tsx
@@ -1,5 +1,6 @@
 import { forwardRef } from 'react'
 import { FormControl, Input } from 'rsc-daisyui'
+import uniqid from 'uniqid'
 import { FormLabel } from '@/components/atoms'
 import { clsx, getLabelText } from '@/lib/utils'
 
@@ -9,22 +10,22 @@ interface FormInputProps extends BaseProps<typeof Input.Inside> {
 
 const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
   ({ $label, $error, $isHalf = true, placeholder = '…', type = 'text', ...rest }, inputRef) => {
-    const labelText = getLabelText($label)
+    const id = uniqid()
 
     return (
       <FormControl className={clsx($isHalf && 'inline-block md:w-1/2')}>
-        <FormLabel for={labelText} text={labelText} />
+        <FormLabel for={id} text={getLabelText($label)} />
         <Input.Inside
           {...rest}
           ref={inputRef}
           className='w-full'
           color={$error && 'error'}
-          id={labelText}
+          id={id}
           placeholder={placeholder}
           size='sm'
           type={type}
         />
-        <FormLabel error={$error} for={labelText} />
+        <FormLabel error={$error} for={id} />
       </FormControl>
     )
   }
--- src/components/molecules/form-select.tsx
+++ src/components/molecules/form-select.tsx
@@ -1,5 +1,6 @@
 import { forwardRef } from 'react'
 import { FormControl, Select } from 'rsc-daisyui'
+import uniqid from 'uniqid'
 import { FormLabel } from '@/components/atoms'
 import { getLabelText } from '@/lib/utils'
 
@@ -9,26 +10,19 @@ interface FormSelectProps extends BaseProps<typeof Select> {
 
 const FormSelect = forwardRef<HTMLSelectElement, FormSelectProps>(
   ({ $label, $options, $error, ...rest }, selectRef) => {
-    const labelText = getLabelText($label)
+    const id = uniqid()
 
     return (
       <FormControl className='inline-block md:w-1/2'>
-        <FormLabel for={labelText} text={labelText} />
-        <Select
-          {...rest}
-          ref={selectRef}
-          className='w-full capitalize'
-          color={$error && 'error'}
-          id={labelText}
-          size='sm'
-        >
+        <FormLabel for={id} text={getLabelText($label)} />
+        <Select {...rest} ref={selectRef} className='w-full capitalize' color={$error && 'error'} id={id} size='sm'>
           {$options.map((option, index) => (
             <option key={index} disabled={index === 0} value={option}>
               {index === 0 ? 'Select' : option}
             </option>
           ))}
         </Select>
-        <FormLabel error={$error} for={labelText} />
+        <FormLabel error={$error} for={id} />
       </FormControl>
     )
   }
