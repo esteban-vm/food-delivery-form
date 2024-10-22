4ef3d210387a41a700939378b649e0dd759fb00c
esteban-vm <estebanvm90@outlook.com>
2024-10-22 09:42:12 -0300

    add sub forms

---
+++ src/components/organisms/checkout-form.tsx
@@ -0,0 +1,31 @@
+import { useFormContext } from 'react-hook-form'
+import { FormRow } from '@/components/atoms'
+import { FormSelect } from '@/components/molecules'
+
+export default function CheckoutForm() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<ICheckoutDetails>()
+
+  return (
+    <FormRow>
+      <FormSelect
+        $error={errors.checkoutDetails?.paymentMethod}
+        $label='checkoutDetails.paymentMethod'
+        $options={['', 'on delivery', 'online']}
+        {...register('checkoutDetails.paymentMethod', {
+          required: 'Payment Method is required',
+        })}
+      />
+      <FormSelect
+        $error={errors.checkoutDetails?.deliveryTime}
+        $label='checkoutDetails.deliveryTime'
+        $options={['', 'Half an Hour', '1 Hour', '2 Hours', '3 Hours']}
+        {...register('checkoutDetails.deliveryTime', {
+          required: 'Delivery Time is required',
+        })}
+      />
+    </FormRow>
+  )
+}
---
+++ src/components/organisms/customer-form.tsx
@@ -0,0 +1,56 @@
+import { useFormContext } from 'react-hook-form'
+import { FaEnvelope, FaPhone, FaUser } from 'react-icons/fa6'
+import { FormRow } from '@/components/atoms'
+import { FormInput } from '@/components/molecules'
+
+export default function CustomerForm() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<ICustomerDetails>()
+
+  return (
+    <>
+      <FormInput
+        $error={errors.customerDetails?.name}
+        $isHalf={false}
+        $label='customerDetails.name'
+        start={<FaUser />}
+        {...register('customerDetails.name', {
+          minLength: { value: 3, message: 'Name must be at least 3 letters long' },
+          maxLength: { value: 50, message: 'Name must be at most 50 letters long' },
+          required: 'Name is required',
+        })}
+      />
+      <FormRow>
+        <FormInput
+          $error={errors.customerDetails?.email}
+          $label='customerDetails.email'
+          placeholder='email@example.com'
+          start={<FaEnvelope />}
+          type='email'
+          {...register('customerDetails.email', {
+            pattern: {
+              value: /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim,
+              message: 'Incorrect email format',
+            },
+            validate: {
+              notFake: (value) => value !== 'email@email.com' || 'This email address is blocked',
+              notBlackListed: (value) => !value.endsWith('example.com') || 'This domain is not supported',
+            },
+            required: 'Email is required',
+          })}
+        />
+        <FormInput
+          $error={errors.customerDetails?.cellphone}
+          $label='customerDetails.cellphone'
+          start={<FaPhone />}
+          type='tel'
+          {...register('customerDetails.cellphone', {
+            required: 'Cellphone is required',
+          })}
+        />
+      </FormRow>
+    </>
+  )
+}
