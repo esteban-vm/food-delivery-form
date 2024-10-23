303b2116b43b21dea1b33931c074bbd9ae0310b3
esteban-vm <estebanvm90@outlook.com>
2024-10-23 12:17:33 -0300

    add form control to button component

--- src/components/atoms/form-button.tsx
+++ src/components/atoms/form-button.tsx
@@ -1,17 +1,21 @@
+import type { Control } from 'react-hook-form'
+import { useFormState } from 'react-hook-form'
 import { FaTruckFast } from 'react-icons/fa6'
 import { Button, FormControl } from 'rsc-daisyui'
 
 interface FormButtonProps {
-  disabled: boolean
+  control: Control<IFoodDeliveryForm>
 }
 
-export default function FormButton(props: FormButtonProps) {
+export default function FormButton({ control }: FormButtonProps) {
+  const { isSubmitting } = useFormState({ control })
+
   return (
     <FormControl className='pointer-events-none mt-4'>
       <Button
-        {...props}
         className='pointer-events-auto mx-auto w-full max-w-sm font-semibold italic text-white'
         color='primary'
+        disabled={isSubmitting}
         size='sm'
         type='submit'
       >
